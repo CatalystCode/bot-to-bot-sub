320dc375a00aff035942a45491956ae42e4ac565
amitu <ami.turgman@gmail.com>
2017-04-06 16:53:29 +0300

    finalizing bot to bot handoff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var builder = require('botbuilder');
+var request = require('request');
 var fs = require('fs');
 var port = process.env.PORT || 3978;
 var app = express();
@@ -8,6 +9,8 @@ var app = express();
 console.log('starting sub bot 1');
 
 var config = require('./config');
+
+var masterBotEndpoint = config.get('MASTER_BOT_ENDPOINT');
 
 app.use((req, res, next) => {
   console.log(`service request for url: '${req.url}'`);
@@ -38,7 +41,17 @@ bot.dialog('/', [
     builder.Prompts.text(session, `** -SlaveBot- **: step ${session.privateConversationData.step++}, enter something....`);
   },
   (session, args, next) => {
-    //session.send(`you typed ${args.response}`);
+    
+    // actively leave the sub bot and resume control for the master bot if user asks to leave
+    if (args.response && args.response.startsWith('leave')) {
+      session.endDialog('returning control to master');
+      var url = masterBotEndpoint + '/api/leave/' + session.message.address.conversation.id;
+      return request.post(url, err => {
+        if (err) console.error(`error in http request: ${url}: ${err}`)
+      });
+    }
+
+    // restart dialog loop
     session.replaceDialog('/', 0);
   }
 ]);
@@ -48,7 +61,6 @@ app.post('/api/messages', connector.listen());
 app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
-
 
 
 module.exports = app;
